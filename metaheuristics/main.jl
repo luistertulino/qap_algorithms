29790f764e75ec90cc4198ac38d580042d8892ea
Luis Tertulino <luistertulino95@gmail.com>
2018-08-27 17:48:03 -0300

    Update main.jl

--- metaheuristics/main.jl
+++ metaheuristics/main.jl
@@ -1,5 +1,11 @@
 
 include("parsefile.jl");
+include("qap_model.jl");
+
 (n, F, D) = parsefile(ARGS[1]);
 
+(obj, gap) = qap_model(n, F, D);
 
+open("results.txt", "a") do f
+    writedlm(f, [split(args, ".dat")[1] obj gap], " ")
+end
