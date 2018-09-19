3b6f60140c95dd519736315acaec65690996106e
Luis Tertulino <luistertulino95@gmail.com>
2018-09-19 00:58:48 -0300

    change gitignore

---
+++ metaheuristics/lib/munkres-cpp-master/build/CMakeFiles/munkres.bin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/munkres.bin.dir/examples/main.cpp.o"
+  "munkres.bin.pdb"
+  "munkres.bin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/munkres.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ metaheuristics/lib/munkres-cpp-master/build/CMakeFiles/munkres.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tertuliano/Programming/qap_algorithms/metaheuristics/lib/munkres-cpp/src/adapters/adapter.cpp" "/home/tertuliano/Programming/qap_algorithms/metaheuristics/lib/munkres-cpp/build/CMakeFiles/munkres.dir/src/adapters/adapter.cpp.o"
+  "/home/tertuliano/Programming/qap_algorithms/metaheuristics/lib/munkres-cpp/src/munkres.cpp" "/home/tertuliano/Programming/qap_algorithms/metaheuristics/lib/munkres-cpp/build/CMakeFiles/munkres.dir/src/munkres.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../src/adapters"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
